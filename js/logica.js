b40a9420c7d8825ad3240b95a737df424cc18ac2
JesusCastro2511 <jedahymc@gmail.com>
2019-06-01 17:27:35 -0500

    nuevos archivos

---
+++ js/logica.js
@@ -0,0 +1,32 @@
+function sumar(a, b) {
+    document.getElementById('sumar').innerHTML = a + b;
+}
+
+function restar(a, b) {
+    document.getElementById('restar').innerHTML = a - b;
+}
+function multiplicar(a, b) {
+    document.getElementById('multiplicar').innerHTML = a * b;
+}
+function dividir(a, b) {
+    document.getElementById('dividir').innerHTML = a / b;
+}
+
+function imprimir() {
+    var text = "";
+    for (var i = 0; i < getCars().length; i++) {
+        text = "<tr>" + "<th>" + text + "<h1>" + getCars()[i] + "</h1>" + "</th>" + "</tr>";
+    }
+    document.getElementById('tabla').innerHTML = text;
+}
+function getCars() {
+    var cars = ["bmw", "volvo", "saab", "ford"];
+    return cars;
+}
+function limpiar() {
+    var text = "";
+    for (var i = 0; i < getCars().length; i++) {
+        text = "<tr>" + "<th>" + text + "<h1>" + text + "</h1>" + "</th>" + "</tr>";
+    }
+    document.getElementById('tabla').innerHTML = text;
+}
